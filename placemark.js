e4dbd9a6b2d8da79fdd32cbdf11614f0721352cd
Dave Harned <dharned@gmail.com>
2017-02-11 20:18:02 -0600

    test text grab solution from SO

--- placemark.js
+++ placemark.js
@@ -1,3 +1,16 @@
 document.addEventListener('click', function(e){
-    console.log(e.target.innerText);
+    var s = window.getSelection();
+    var range = s.getRangeAt(0);
+    var node = s.anchorNode;
+    while (range.toString().indexOf(' ') != 0) {
+        range.setStart(node, (range.startOffset - 1));
+    }
+    range.setStart(node, range.startOffset + 1);
+    do {
+        range.setEnd(node, range.endOffset + 1);
+
+    } while (range.toString().indexOf(' ') == -1 && range.toString().trim() != '' && range.endOffset < node.length);
+    var str = range.toString().trim();
+
+    console.log(str);
 })
