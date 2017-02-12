eb65294f2fb6d99c6a545d925205d2d6d3ea2749
Dave Harned <dharned@gmail.com>
2017-02-11 21:54:11 -0600

    add span to clicked el

--- placemark.js
+++ placemark.js
@@ -1,21 +1,39 @@
-function captureWord() {
+var openSpan = '<span class="placemark">';
+var closeSpan = '</span>';
+
+function captureWord(e) {
     var s = window.getSelection();
     var range = s.getRangeAt(0);
     var node = s.anchorNode;
     while (range.toString().indexOf(' ') != 0) {
         range.setStart(node, (range.startOffset - 1));
     }
+
     range.setStart(node, range.startOffset + 1);
+    
     do {
         range.setEnd(node, range.endOffset + 1);
-
     } while (range.toString().indexOf(' ') == -1 && range.toString().trim() != '' && range.endOffset < node.length);
+    
     var str = range.toString().trim();
-
-    return str;
+    var newhtml = e.target.innerHTML.replace(str, addSpan(str));
+    e.target.innerHTML = newhtml;
 }
 
-document.addEventListener('click', function(){
-    var selectedWord = captureWord();
-    console.log(selectedWord);
+document.addEventListener('click', function(e){
+    var wordAndElement = captureWord(e);
+    // wrapWords(selectedWord);
+    console.log(wordAndElement);
 })
+
+function addSpan(str) {
+    console.log(openSpan + str + closeSpan);
+    return openSpan + str + closeSpan;
+}
+
+function removeSpan(str) {
+    var str1 = str.replace(openSpan, "");
+    var str2 = str1.replace(closeSpan, "");
+    return str2;
+}
+
