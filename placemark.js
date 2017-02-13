8fce62d9d42a19c5951fc98175ede2d733a648db
Dave Harned <dharned@gmail.com>
2017-02-12 20:44:28 -0600

    separate getClickedWord code and comment

--- placemark.js
+++ placemark.js
@@ -1,33 +1,43 @@
 var openSpan = '<span class="placemark">';
 var closeSpan = '</span>';
 
-function captureWord(e) {
-    var s = window.getSelection();
-    var range = s.getRangeAt(0);
-    var node = s.anchorNode;
+function getClickedWord(range, node) {
+    // count back chars until you reach whitespace, set range start
     while (range.toString().indexOf(' ') != 0) {
         range.setStart(node, (range.startOffset - 1));
     }
-
     range.setStart(node, range.startOffset + 1);
     
+    // count forward chars until you reach whitespace, set range end
     do {
         range.setEnd(node, range.endOffset + 1);
     } while (range.toString().indexOf(' ') == -1 && range.toString().trim() != '' && range.endOffset < node.length);
     
     var str = range.toString().trim();
-    var newhtml = e.target.innerHTML.replace(str, addSpan(str));
-    e.target.innerHTML = newhtml;
+    console.log(str);
+}
+
+function handleClick(e) {
+    // capture click location
+    var s = window.getSelection();
+    var range = s.getRangeAt(0);
+    var node = s.anchorNode;
+
+    getClickedWord(range, node);
+    
+
+    // var newhtml = e.target.innerHTML.replace(str, addSpan(str));
+    // e.target.innerHTML = newhtml;
 }
 
 document.addEventListener('click', function(e){
-    var wordAndElement = captureWord(e);
+    var wordAndElement = handleClick(e);
     // wrapWords(selectedWord);
-    console.log(wordAndElement);
+    // console.log(wordAndElement);
 })
 
 function addSpan(str) {
-    console.log(openSpan + str + closeSpan);
+    // console.log(openSpan + str + closeSpan);
     return openSpan + str + closeSpan;
 }
 
